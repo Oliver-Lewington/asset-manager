4d9a98e24fcf680c94f92c91ceaae7d25ba5b382
Oliver Lewington <olliedalpha@gmail.com>
2024-12-12 17:15:50 +0000

    Navigation bar alterations. Imrpvoving application design

--- asset_manager/inventory/urls.py
+++ asset_manager/inventory/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name=''),
     path('assets/', views.assets, name='assets'),
     path('customer/', views.customer, name='customer'),
+
+    path('login/', views.customer, name='login'),
+    path('register/', views.customer, name='register'),
 ]
