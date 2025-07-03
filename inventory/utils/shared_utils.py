66d1dfb65928c377d9a5fa4666b6a0fdc6e40f08
Oliver Lewington <oliver.lewington@eviden.com>
2025-07-03 23:23:29 +0100

    Commiting final changes to application adding messages to inform user of RBAC + Adding faked data

--- inventory/utils/shared_utils.py
+++ inventory/utils/shared_utils.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib import messages
 
+from functools import wraps
 import hashlib
 
 # --- Redirection Helper Functions ---
@@ -16,4 +17,20 @@ def hash_username(username):
     return hashlib.sha256(username.encode('utf-8')).hexdigest()
 
 def staff_required(view_func):
-    return user_passes_test(lambda u: u.is_active and u.is_staff)(view_func)
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        print(f"User: {request.user}, Authenticated: {request.user.is_authenticated}, Staff: {request.user.is_staff}")
+        if not request.user.is_authenticated:
+            messages.error(request, "You must be logged in to access this page. Please log in below.")
+            return redirect('login')
+        if not request.user.is_staff:
+            from django.contrib.auth import logout
+            logout(request)
+            messages.error(
+                request,
+                "Access denied: You do not have sufficient permissions to access this page. "
+                "If you believe this is an error, please contact a system administrator."
+            )
+            return redirect('login')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
